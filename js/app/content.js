68d8854b7fd1b15391882a1b9341f40e457b78c6
neetesh-hinkhoj <neetesh.kumar@hinkhoj.com>
2016-03-03 02:24:40 +0530

    Data Remove Newlines

--- js/app/content.js
+++ js/app/content.js
@@ -1,5 +1,5 @@
 ﻿(function() {
- 
+
 
 
 
@@ -53,7 +53,7 @@ app.service('pageInfoService', function() {
                         model.title = myjson[iN].data.swfcfg.args.title;
                         model.thumbnail = myjson[iN].data.swfcfg.args.thumbnail_url
                         model.links = linkGenerator(myjson[iN].data.swfcfg);
-                        out(myjson);
+
                         break;
                      }
 
@@ -61,10 +61,10 @@ app.service('pageInfoService', function() {
                 }
                 catch(err) {
                     out(err.message);
-                    
+
                 }
-                
-               
+
+
                 // out(model.links)
                 //
                 callback(model);
@@ -82,13 +82,13 @@ app.service('pageInfoService', function() {
 
 app.controller("cs_myCtrl",["$scope","pageInfoService",function($scope, pageInfoService) {
     $scope.message = document.URL;
-    
+
     if (~$scope.message.indexOf('www.youtube.com/watch?v=')) {
         pageInfoService.getInfo(function(info) {
-            // To call the digest cycle either use $apply or put empty ng-click on div 
+            // To call the digest cycle either use $apply or put empty ng-click on div
             ///Both Works Fine
             $scope.$apply(function() {
-                
+
                 $scope.data = {
                     url: info.url,
                     title: info.title,
@@ -103,19 +103,19 @@ app.controller("cs_myCtrl",["$scope","pageInfoService",function($scope, pageInfo
 
     if (~$scope.message.indexOf('www.youtube.com/playlist?list=')) {
         $scope.playlinks = "";
-       
+
         $scope.doSomething = function() {
             chrome.runtime.sendMessage({
                 type: 'copy',
                 text: $scope.playlinks
             });
-            
+
             $('#copy-button').addClass("animated bounceInRight");
             $scope.completed = "Copied to Clipboard";
 
         }
 
-        
+
         var vid = [];
         if (pv != null)
             for (i = 0; i < pv.children.length; i++) {
@@ -124,7 +124,7 @@ app.controller("cs_myCtrl",["$scope","pageInfoService",function($scope, pageInfo
             }
         $scope.len = pv.children.length;
         $scope.completed = 0;
-       
+
         var asyncLoop = function(o) {
             var i = -1,
                 length = o.length;
@@ -150,7 +150,7 @@ app.controller("cs_myCtrl",["$scope","pageInfoService",function($scope, pageInfo
                     pageInfoService.getInfo(function(info) {
 
                         $scope.$apply(function() {
-                            
+
                             $scope.data = {
                                     url: info.url,
                                     title: info.title,
@@ -200,21 +200,21 @@ function linkGenerator(data) {
         resolution[r[i].split('/')[0]] = r[i].split('/')[1];
 
     };
-
-    html5version = html5version.split('/html5player')[1];
-    html5version = html5version.slice(1, html5version.length);
+    // html5version = html5version.split('/html5player')[1];
+    //
+    // html5version = html5version.slice(1, html5version.length);
 
 
     if (typeof(Storage) !== "undefined") {
         var stored_version = localStorage.getItem("html5version");
-        if (stored_version == null || stored_version != html5version) {           
+        if (stored_version == null || stored_version != html5version) {
             var sig = decryptSignature(html5versionlink);
             localStorage.setItem("html5version", html5version);
-            localStorage.setItem("sig", sig);            
+            localStorage.setItem("sig", sig);
         }
 
     } else {
-        
+
     }
 
     links = links.split(',');
@@ -266,9 +266,9 @@ function linkGenerator(data) {
         }
         dl[j].url = dl[j].url + "&title=" + encodeURIComponent(title.replace(/"/g, "'"));
 
-        dl[j].resolution = resolution[dl[j].itag];    
+        dl[j].resolution = resolution[dl[j].itag];
 
-    }    
+    }
     return dl;
 
 }
@@ -279,35 +279,22 @@ function decryptSignature(url) {
         type: 'GET',
         url: url,
         success: function(data) {
-
-            var patt = /function\s[a-zA-Z0-9$_]{2,4}\([a-z]\)\s{0,2}\{[a-z]=[a-z]\.split\(""\);[a-zA-Z0-9.(),;$_]{1,300}return\s[a-z]\.join\(""\)}/g;
+            data = data.replace(/(\r\n|\n|\r)/gm,"");
+            var patt = /function[a-zA-Z0-9$_\(\)]{1,3}\{[a-z]=[a-z]\.split\(""\);[a-zA-Z0-9.\(,\);]{1,300}return\s[a-z].join\(""\)}/g;
             var algo = patt.exec(data);
-            // out(algo);
-
             var funcfinder = /var\s[a-zA-Z0-9$_]{1,3}=\{[a-zA-Z0-9]{1,3}:function[a-zA-Z0-9.(),;:{}\s=\[\]%]{1,150}\}\;/g;
             var func = funcfinder.exec(data);
-            // out(func);
-            console.log(func);
-            console.log(algo);
-
-
             var functionName = [];
             funcfinder = /([a-zA-Z0-9]{1,3})(?=\:)/g;
             var m;
             while (m = funcfinder.exec(func[0])) {
                 functionName.push(m[0]);
             }
-            // console.log(functionName);
-
             var functionPos = new Array(3);
             functionPos[0] = func[0].indexOf("reverse");
             functionPos[1] = func[0].indexOf("%");
             functionPos[2] = func[0].indexOf("splice");
-            // out(functionPos);
-
-            var funcMap = new Array(3);
             for (var i = 0; i < 3; i++) {
-
                 var ans = 0;
                 var min = 999999999;
                 for (var j = 0; j < 3; j++) {
@@ -318,36 +305,27 @@ function decryptSignature(url) {
                     }
                 }
                 funcMap[i] = ans;
-
             }
-            // out(funcMap);
             var mp = {};
             for (var i = 0; i < 3; i++) {
                 mp[functionName[i]] = funcMap[i];
 
             }
-            // out(mp);
             var sf = algo[0].split(";");
-            // out(sf);
-
             for (var i = 1; i < sf.length - 1; i++) {
                 var functioncall = /\.[0-9a-zA-Z_$]{1,2}/g;
                 var num = /[0-9]{1,2}\)/g;
                 var f = functioncall.exec(sf[i]);
                 var n = num.exec(sf[i]);
-
                 var key = f[0].slice(1, f[0].length);
                 var val = n[0].slice(0, n[0].length - 1);
                 sig += mp[key] + ":" + val + " ";
             }
-
         },
         data: {},
         async: false
     });
-    
     return sig.trim();
-
 }
 
 })();
